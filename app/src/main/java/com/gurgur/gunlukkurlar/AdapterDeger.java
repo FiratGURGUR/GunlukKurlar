a053ab8a40ea9a09e92565fdccbd24bbc1714267
Firat <frtgurgur@gmail.com>
2019-10-20 02:09:38 +0300

    Ağ bağlantısı kontrolü eklendi

--- app/src/main/java/com/gurgur/gunlukkurlar/AdapterDeger.java
+++ app/src/main/java/com/gurgur/gunlukkurlar/AdapterDeger.java
@@ -1,18 +1,26 @@
 package com.gurgur.gunlukkurlar;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class AdapterDeger extends RecyclerView.Adapter<AdapterDeger.ViewHolder> {
-
+    String charString="";
+    String searchString="";
     public static class ViewHolder extends RecyclerView.ViewHolder
     {
         public TextView BirimAdi;
@@ -30,10 +38,12 @@ public class AdapterDeger extends RecyclerView.Adapter<AdapterDeger.ViewHolder> 
     }
 
     private List<ModelDeger> user_list;
+    public static List<ModelDeger> user_listfiltered;
     private Context context;
 
     AdapterDeger(List<ModelDeger> user_list, Context context){
         this.user_list = user_list;
+        this.user_listfiltered = user_list;
         this.context = context;
     }
 
@@ -45,28 +55,70 @@ public class AdapterDeger extends RecyclerView.Adapter<AdapterDeger.ViewHolder> 
         final  ViewHolder view_holder = new ViewHolder(vr);
 
 
-
         return view_holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        holder.BirimAdi.setText(user_list.get(position).getBirimAdi());
-        holder.BirimAlis.setText(user_list.get(position).getBirimAlis());
-        holder.BirimSatis.setText(user_list.get(position).getBirimSatis());
+        holder.BirimAdi.setText(user_listfiltered.get(position).getBirimAdi());
+        holder.BirimAlis.setText( "Alış: "+user_listfiltered.get(position).getBirimAlis());
+        holder.BirimSatis.setText("Satış: "+user_listfiltered.get(position).getBirimSatis());
 
 
+        String name = user_listfiltered.get(position).getBirimAdi().toLowerCase(Locale.getDefault());
+
+        if (name.contains(searchString)) {
+
+            int startPos = name.indexOf(searchString);
+            int endPos = startPos + searchString.length();
+
+            Spannable spanString = Spannable.Factory.getInstance().newSpannable(holder.BirimAdi.getText());
+            spanString.setSpan(new ForegroundColorSpan(Color.BLUE), startPos, endPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            spanString.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), startPos, endPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            holder.BirimAdi.setText(spanString);
+        }
 
 
     }
 
     @Override
     public int getItemCount() {
-        return user_list.size();
+        return user_listfiltered.size();
     }
 
-
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                charString = charSequence.toString();
+                searchString = charString;
+                if (charString.isEmpty()) {
+                    user_listfiltered = user_list;
+                } else {
+                    List<ModelDeger> filteredList = new ArrayList<>();
+                    for (ModelDeger row : user_list) {
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if (row.getBirimAdi().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+                    user_listfiltered = filteredList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = user_list;
+                return filterResults;
+            }
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                user_list = (ArrayList<ModelDeger>) filterResults.values;
+                // refresh the list with filtered data
+                notifyDataSetChanged();
+            }
+        };
+    }
 
 
 }
--- app/src/main/java/com/gurgur/gunlukkurlar/MainActivity.java
+++ app/src/main/java/com/gurgur/gunlukkurlar/MainActivity.java
@@ -2,11 +2,19 @@ package com.gurgur.gunlukkurlar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -26,20 +34,29 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    AdapterDeger mAdapter;
-    List<ModelDeger> mList;
-    TextView updateDate;
+   static RecyclerView recyclerView;
+    static  AdapterDeger mAdapter;
+    static  List<ModelDeger> mList;
+    static TextView updateDate;
+    static EditText search;
+    static ImageView conn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        conn=findViewById(R.id.imageView);
+        search = findViewById(R.id.editText);
         recyclerView = findViewById(R.id.degerr);
         mList = new ArrayList<>();
         updateDate = findViewById(R.id.lastUpdate);
 
-        GetDatas();
+
+
+
+
+
+
     }
 
     public void AutoAdd(JSONObject jsonObject,String adi){
@@ -54,102 +71,137 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GetDatas();
+    }
+
+
 
     public void GetDatas(){
-        Retrofit.Builder builder = new Retrofit.Builder()
-                .baseUrl("https://finans.truncgil.com/")
-                .addConverterFactory(GsonConverterFactory.create());
-        Retrofit retrofit = builder.build();
-        DovizApiInterface client = retrofit.create(DovizApiInterface.class);
 
-        Call<ResponseBody> call = client.OgrencileriAl();
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+        if (isNetworkConnected()){
+            conn.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+            Retrofit.Builder builder = new Retrofit.Builder()
+                    .baseUrl("https://finans.truncgil.com/")
+                    .addConverterFactory(GsonConverterFactory.create());
+            Retrofit retrofit = builder.build();
+            DovizApiInterface client = retrofit.create(DovizApiInterface.class);
 
-                try {
+            Call<ResponseBody> call = client.OgrencileriAl();
+            call.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
 
-                    JSONObject jsonObject =new JSONObject(response.body().string());
+                    try {
+
+                        JSONObject jsonObject =new JSONObject(response.body().string());
 
                 /*    JSONObject jsonAbdDolar = new JSONObject(jsonObject.getString("ABD DOLARI"));
                     mList.add(new ModelDeger("ABD DOLARI",jsonAbdDolar.getString("Alış"),jsonAbdDolar.getString("Satış"),jsonAbdDolar.getString("Tür")));
 */
 
-                    updateDate.setText("Son Güncelleme : " + jsonObject.getString("Güncelleme Tarihi"));
+                        updateDate.setText("Son Güncelleme : " + jsonObject.getString("Güncelleme Tarihi"));
 
-                    AutoAdd(jsonObject,"ABD DOLARI");
-                    AutoAdd(jsonObject,"AVUSTRALYA DOLARI");
-
-
-                    AutoAdd(jsonObject,"DANİMARKA KRONU");
-                    AutoAdd(jsonObject,"EURO");
-                    AutoAdd(jsonObject,"İNGİLİZ STERLİNİ");
-                    AutoAdd(jsonObject,"İSVİÇRE FRANGI");
-                    AutoAdd(jsonObject,"İSVEÇ KRONU");
-                    AutoAdd(jsonObject,"KANADA DOLARI");
-                    AutoAdd(jsonObject,"KUVEYT DİNARI");
-                    AutoAdd(jsonObject,"NORVEÇ KRONU");
-                    AutoAdd(jsonObject,"SUUDİ ARABİSTAN RİYALİ");
-                    AutoAdd(jsonObject,"JAPON YENİ");
-                    AutoAdd(jsonObject,"BULGAR LEVASI");
-                    AutoAdd(jsonObject,"RUMEN LEYİ");
-                    AutoAdd(jsonObject,"RUS RUBLESİ");
-                    AutoAdd(jsonObject,"İRAN RİYALİ");
-                    AutoAdd(jsonObject,"ÇİN YUANI");
-                    AutoAdd(jsonObject,"PAKİSTAN RUPİSİ");
-                    AutoAdd(jsonObject,"KATAR RİYALİ");
-                    AutoAdd(jsonObject,"Çeyrek Altın");
-                    AutoAdd(jsonObject,"Yarım Altın");
-                    AutoAdd(jsonObject,"Tam Altın");
-                    AutoAdd(jsonObject,"Cumhuriyet Altını");
-                    AutoAdd(jsonObject,"Ons");
-                    AutoAdd(jsonObject,"Gram Altın");
-                    AutoAdd(jsonObject,"Ata Altın");
-                    AutoAdd(jsonObject,"14 Ayar Altın");
-                    AutoAdd(jsonObject,"18 Ayar Altın");
-                    AutoAdd(jsonObject,"22 Ayar Bilezik");
-                    AutoAdd(jsonObject,"İkibuçuk Altın");
-                    AutoAdd(jsonObject,"Beşli Altın");
-                    AutoAdd(jsonObject,"Gremse Altın");
-                    AutoAdd(jsonObject,"Gümüş");
-
-                    mAdapter =new AdapterDeger(mList,MainActivity.this);
+                        AutoAdd(jsonObject,"ABD DOLARI");
+                        AutoAdd(jsonObject,"AVUSTRALYA DOLARI");
 
 
-                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
-                    linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-                    linearLayoutManager.scrollToPosition(0);
-                    recyclerView.setLayoutManager(linearLayoutManager);
+                        AutoAdd(jsonObject,"DANİMARKA KRONU");
+                        AutoAdd(jsonObject,"EURO");
+                        AutoAdd(jsonObject,"İNGİLİZ STERLİNİ");
+                        AutoAdd(jsonObject,"İSVİÇRE FRANGI");
+                        AutoAdd(jsonObject,"İSVEÇ KRONU");
+                        AutoAdd(jsonObject,"KANADA DOLARI");
+                        AutoAdd(jsonObject,"KUVEYT DİNARI");
+                        AutoAdd(jsonObject,"NORVEÇ KRONU");
+                        AutoAdd(jsonObject,"SUUDİ ARABİSTAN RİYALİ");
+                        AutoAdd(jsonObject,"JAPON YENİ");
+                        AutoAdd(jsonObject,"BULGAR LEVASI");
+                        AutoAdd(jsonObject,"RUMEN LEYİ");
+                        AutoAdd(jsonObject,"RUS RUBLESİ");
+                        AutoAdd(jsonObject,"İRAN RİYALİ");
+                        AutoAdd(jsonObject,"ÇİN YUANI");
+                        AutoAdd(jsonObject,"PAKİSTAN RUPİSİ");
+                        AutoAdd(jsonObject,"KATAR RİYALİ");
+                        AutoAdd(jsonObject,"Çeyrek Altın");
+                        AutoAdd(jsonObject,"Yarım Altın");
+                        AutoAdd(jsonObject,"Tam Altın");
+                        AutoAdd(jsonObject,"Cumhuriyet Altını");
+                        AutoAdd(jsonObject,"Ons");
+                        AutoAdd(jsonObject,"Gram Altın");
+                        AutoAdd(jsonObject,"Ata Altın");
+                        AutoAdd(jsonObject,"14 Ayar Altın");
+                        AutoAdd(jsonObject,"18 Ayar Altın");
+                        AutoAdd(jsonObject,"22 Ayar Bilezik");
+                        AutoAdd(jsonObject,"İkibuçuk Altın");
+                        AutoAdd(jsonObject,"Beşli Altın");
+                        AutoAdd(jsonObject,"Gremse Altın");
+                        AutoAdd(jsonObject,"Gümüş");
+
+                        mAdapter =new AdapterDeger(mList,MainActivity.this);
+
+
+                        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+                        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                        linearLayoutManager.scrollToPosition(0);
+                        recyclerView.setLayoutManager(linearLayoutManager);
+
+                        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                                linearLayoutManager.getOrientation());
+                        recyclerView.addItemDecoration(dividerItemDecoration);
+
+                        recyclerView.setHasFixedSize(true);
+                        recyclerView.setAdapter(mAdapter);
+                        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+                        search.addTextChangedListener(new TextWatcher() {
+                            @Override
+                            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                            }
+                            @Override
+                            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                            }
+                            @Override
+                            public void afterTextChanged(Editable s) {
+                                mAdapter.getFilter().filter(s.toString());
+                            }
+                        });
 
 
 
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
-
-                    recyclerView.setHasFixedSize(true);
-                    recyclerView.setAdapter(mAdapter);
-                    recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-
-
-
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
 
-            }
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
 
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                }
+            });
+        }else{
+            recyclerView.setVisibility(View.GONE);
+            conn.setVisibility(View.VISIBLE);
+        }
 
-            }
-        });
+
+
 
 
     }
 
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
 
 }
